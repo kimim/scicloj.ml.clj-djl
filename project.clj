c82269fc44b9d24cdca425131bad93d095cfbf47
kimim <kimi.im@outlook.com>
2021-10-31 21:13:34 +0800

    fix clojars.org project prefix

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject scicloj/scicloj.ml.clj-djl "0.1.6"
-  :description "clj-djl modles for metamorph.ml and scicloj.ml"
+(defproject org.scicloj/scicloj.ml.clj-djl "0.1.6"
+  :description "clj-djl models for metamorph.ml and scicloj.ml"
   :url "http://github.com/scicloj/scicloj.ml.clj-djl"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [scicloj/metamorph.ml "0.3.0-alpha3"]]
+                 [scicloj/metamorph.ml "0.4.0"]]
   :plugins [[lein-localrepo "0.5.4"]]
   :source-paths ["src"])
