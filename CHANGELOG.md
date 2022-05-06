8df27457b2da21800a20ef74087d7d3331fa603d
Carsten Behring <carsten.behring@gmail.com>
2022-05-06 22:43:21 +0200

    version 1.1.11

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Change Log
+
+## unreleased
+
+- added LDA QDA RDA FLD]
+
+## 0.1.11
+
+- fixed predictions with un-predicted labels
--- build.clj
+++ build.clj
@@ -6,7 +6,7 @@
 (def lib 'org.scicloj/scicloj.ml.clj-djl)
 ; alternatively, use MAJOR.MINOR.COMMITS:
 ;;(def version (format "0.2.%s" (b/git-count-revs nil)))
-(def version "0.1.10")
+(def version "0.1.11")
 
 (defn test "Run the tests." [opts]
   (bb/run-tests opts))
